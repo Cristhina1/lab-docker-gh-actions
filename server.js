5767b5fb2da86a2018edb26dd64d131b3d18cc2e
Cristhina1 <U22327233@utp.edu.pe>
2025-11-27 12:39:05 -0500

    s

--- server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const fs = require('fs');
-http.createServer((req, res) => {
-fs.readFile('index.html', (err, data) => {
-res.writeHead(200, {'Content-Type':'text/html'});
-res.end(data);
-});
-}).listen(3000, () => console.log("Servidor en http://localhost:3000"));
